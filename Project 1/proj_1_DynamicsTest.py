138278ee38f64f80cb40f74ca24e32064277b2b9
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-10-30 14:05:47 -0700

    Edit function description

--- Project 1/proj_1_DynamicsTest.py
+++ Project 1/proj_1_DynamicsTest.py
@@ -31,7 +31,7 @@ def population_model_veloc_function(b, d):
 
 def dynamics_exact_solve(f, d = 1, t_0 = 0.0, s_0 = 1.0, h = 0.1, N = 100):
 	"""
-	Produce time and value array for a given exact solution function(s_0, t_0, t)
+	Produce time and value array for a given exact solution function(s_0, t_0, t).
 
 	Args:
 		f (function): function(s_0, t_0, t) to produce exact values for
@@ -59,4 +59,43 @@ def dynamics_exact_solve(f, d = 1, t_0 = 0.0, s_0 = 1.0, h = 0.1, N = 100):
 	for n in range(N + 1):
 		S[n] = f(s_0, t_0, T[n])
 	
-	return T, S
+	return T, S 
+
+def dynamics_display_step_size_plot(method):
+	# Define Constants
+	b = 1.5
+	d = 1
+	p_0 = 1000
+	t_total = 10
+	step_size = [1, 0.5, 0.1, 0.05, 0.01]
+	colors = ["#0000FF", "#0040C0", "#008080", "#00C040", "#00FF00", "#00FF00"]
+
+	# Create numpy arrays containing the exact solutions
+	p_exact = population_model_exact_sol(b, d)
+	t_exact, s_exact = dynamics_exact_solve(p_exact, s_0 = p_0)
+
+	# Define the population model velocity function
+	p_model = population_model_veloc_function(b, d)
+
+	# Create numpy arrays containing numerical solutions for different step sizes. 
+	t_euler = {}
+	s_euler = {}
+	for h in step_size:
+		t, s = dynamics_solve(p_model, s_0 = p_0, h = h, N = int(t_total/h), method = method)
+		t_euler[h] = t
+		s_euler[h] = s
+
+	# Plot all of the numerical model solutions
+	for i, h in enumerate(step_size):
+		plt.plot(t_euler[h], s_euler[h], color = colors[i])
+
+	# Plot exact solution
+	plt.plot(t_exact, s_exact, color = "red", linestyle = "--")
+
+	# Define Labels
+	plt.xlabel('Time')
+	plt.ylabel('Population')
+	plt.title('Population Model Solutions for Different Step Sizes')
+
+	# Display Plot
+	plt.show()
