12d4bc792982dd7bcc323a07bcaf619e0f31e82c
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-10-30 15:27:39 -0700

    Move plot error to a function

--- Project 1/proj_1_DynamicsTest.py
+++ Project 1/proj_1_DynamicsTest.py
@@ -99,3 +99,36 @@ def dynamics_display_step_size_plot(method):
 
 	# Display Plot
 	plt.show()
+
+def dynamics_display_plot_error(method):
+	# Define Constants
+	b = 1.5
+	d = 1
+	p_0 = 1000
+	t_total = 100
+	h = 0.01
+
+	# Create numpy arrays containing the exact solutions
+	p_exact = population_model_exact_sol(b, d)
+	t_exact, s_exact = dynamics_exact_solve(p_exact, s_0 = p_0, h = h, N = int(t_total/h))
+
+	# Define the population model velocity function
+	p_model = population_model_veloc_function(b, d)
+
+	# Create numpy arrays containing numerical solutions for different methods. 
+	t_euler, s_euler = dynamics_solve(p_model, s_0 = p_0, h = h, N = int(t_total/h), method = method)
+
+	# Plot all of the model solutions
+	plt.plot(t_euler, s_euler, color = "blue")
+	plt.plot(t_exact, s_exact, color = "red", linestyle = "--")
+
+	# Define Labels
+	plt.xlabel('Time')
+	plt.ylabel('Population')
+	plt.title('Numerical vs. Exact Solutions for Long Time Period')
+
+	# Display Plot
+	plt.show()
+
+	# Calculate percent error after t = 100
+	print(f"{round(100 * (s_exact[-1] - s_euler[-1]) / s_exact[-1], 2)}% Error after t = 100.")
