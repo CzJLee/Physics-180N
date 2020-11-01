c5390c8d248a169408ec829f71944eaace6b4545
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-11-01 15:14:18 -0800

    ELLIPTICAL MOTION FTW

--- Project 1/proj_1_ClassicalAnalysis.py
+++ Project 1/proj_1_ClassicalAnalysis.py
@@ -19,22 +19,31 @@ def kepler_d_hamiltonian(m_1, m_2):
 	m = m_1 * m_2 / (m_1 + m_2)
 	
 	# Gravitational Constant
-	# g = 6.67408e-11
-	k = 1
+	g = 6.67408e-11
+	# g = 0.0001
+	k = g * m_1 * m_2
 
-	return lambda x, p: np.array([k * x[0] / ((x[0] ** 2 + x[1] ** 2) ** 1.5), k * x[1] / ((x[0] ** 2 + x[1] ** 2) ** 1.5)]), lambda x, p: p / m
+	return lambda x, p: np.array([k * x[0] / ((x[0] ** 2 + x[1] ** 2) ** 1.5), k * x[1] / ((x[0] ** 2 + x[1] ** 2) ** 1.5)]), lambda x, p: p / m, m, k
 
 # Define using earth masses
-m_sun = 2
-m_earth = 1
-d_qH, d_pH = kepler_d_hamiltonian(m_sun, m_earth)
+m_sun = 1.989e30
+m_earth = 5.972e24
+d_qH, d_pH, m, k = kepler_d_hamiltonian(m_sun, m_earth)
 
-# https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
+# Set initial conditions
 # Consider coordinates at Aphelion
-q_0 = np.array([1, 0])
-p_0 = np.array([0, 1])
+# https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
+q_0 = np.array([152.10e9, 0])
+p_0 = np.array([0, 29290 * m])
 
-t, q, p = hamiltonian_solve(d_qH, d_pH, d = 2, t_0 = 0.0, q_0 = q_0, p_0 = p_0, h = 0.01, N = 10000, method = "Euler")
+# Verify elliptical motion
+energy = (p_0[0] ** 2 + p_0[1] ** 2) / (2 * m) - k / ((q_0[0] ** 2 + q_0[1] ** 2) ** 0.5)
+print(f"Energy is : {energy}")
+l = 152.10e9 * 29290 * m
+eccentricity = (1 + (2 * energy * l ** 2)/(m * k ** 2)) ** 0.5
+print(f"Eccentricity is : {eccentricity}")
+
+t, q, p = hamiltonian_solve(d_qH, d_pH, d = 2, t_0 = 0.0, q_0 = q_0, p_0 = p_0, h = 1000, N = 1000000, method = "Euler")
 
 r_earth = (- m_sun / (m_earth + m_sun)) * q
 
@@ -43,9 +52,3 @@ x, y = r_earth.T
 
 plt.plot(x, y)
 plt.show()
-
-print(p_0)
-print(q_0)
-
-print(d_pH(q_0, p_0))
-print(d_qH(q_0, p_0))
