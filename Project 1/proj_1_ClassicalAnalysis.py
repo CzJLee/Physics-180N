ff129361b9da311cb1a961a48e93336a84a0a1c9
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-11-01 13:07:27 -0800

    I am getting orbital motion now!

--- Project 1/proj_1_ClassicalAnalysis.py
+++ Project 1/proj_1_ClassicalAnalysis.py
@@ -19,27 +19,29 @@ def kepler_d_hamiltonian(m_1, m_2):
 	m = m_1 * m_2 / (m_1 + m_2)
 	
 	# Gravitational Constant
-	g = 6.67408e-11
-	k = g * m_1 * m_2
+	# g = 6.67408e-11
+	k = 1
 
 	return lambda x, p: np.array([k * x[0] / ((x[0] ** 2 + x[1] ** 2) ** 1.5), k * x[1] / ((x[0] ** 2 + x[1] ** 2) ** 1.5)]), lambda x, p: p / m
 
 # Define using earth masses
-m_sun = 333000
+m_sun = 2
 m_earth = 1
 d_qH, d_pH = kepler_d_hamiltonian(m_sun, m_earth)
 
 # https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
 # Consider coordinates at Aphelion
-q_0 = (- (m_earth + m_sun) / m_sun ) * np.array([152.1e9, 0])
-p_0 = (- (m_earth + m_sun) / m_sun ) * np.array([0, 29.78e3*m_earth])
+q_0 = np.array([1, 0])
+p_0 = np.array([0, 1])
 
-t, q, p = hamiltonian_solve(d_qH, d_pH, d = 2, t_0 = 0.0, q_0 = q_0, p_0 = p_0, h = 86400, N = 100, method = "Euler")
+t, q, p = hamiltonian_solve(d_qH, d_pH, d = 2, t_0 = 0.0, q_0 = q_0, p_0 = p_0, h = 0.01, N = 10000, method = "Euler")
 
 r_earth = (- m_sun / (m_earth + m_sun)) * q
 
-print(r_earth)
-plt.plot(r_earth)
+# Unpack (x, y) coordinates to be plotted using matplotlib
+x, y = r_earth.T
+
+plt.plot(x, y)
 plt.show()
 
 print(p_0)
