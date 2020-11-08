eb900f5b82d125ff08441bbc4430eb2c76d57421
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-11-08 08:47:36 -0800

    Code Tests, Bult testing methods

---
+++ Project 2/proj_2_EigenTest.py
@@ -0,0 +1,28 @@
+import numpy as np
+from math import sqrt
+# https://numpy.org/doc/stable/reference/generated/numpy.linalg.eigh.html
+from numpy.linalg import eigh
+from scipy.stats import unitary_group
+
+def npprint(a, text = None):
+	if text:
+		print(text)
+	with np.printoptions(precision=4, suppress=True, formatter={'float': '{:0.3f}'.format}, linewidth=100):
+		print(a)
+
+def gen_rand_herm(dim = 3, eigenvalues = None):
+	if eigenvalues is None:
+		# Generate an array of random integers to use as random eigenvalues
+		# I will sample integers from the range [-100, 101). 
+		eigenvalues = np.random.randint(low = -100, high = 101, size = dim)
+
+	# Now make a diagonal matrix with the eigenvalues as its diagonal elements. 
+	a = np.zeros((dim, dim), int)
+	np.fill_diagonal(a, eigenvalues)
+
+	# Apply a similarity transformation by a unitary matrix to get a non diagonal matrix with the same eigenvalues
+	# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.unitary_group.html
+	x = unitary_group.rvs(dim)
+	a = np.linalg.multi_dot((x, a, x.conj().T))
+
+	return eigenvalues, a
