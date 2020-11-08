20c0fbff5403ba03b25ac2c4dd967892cb750fb8
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-11-08 15:41:42 -0800

    Tests

--- Project 2/test2.py
+++ Project 2/test2.py
@@ -1,13 +1,21 @@
-from proj_2_EigenTest import gen_rand_herm, npprint
+from proj_2_EigenTest import gen_rand_herm, npprint, is_hermitian
 import numpy as np
 from math import sqrt
-from test import jacobi
+from proj_2_module import jacobi, hermitian_matrix_split
 
-a = np.array([[1,sqrt(2),2],[sqrt(2),3,sqrt(2)],[2,sqrt(2),1]])
+
+
+test_array = np.array([[1,sqrt(2),4],[sqrt(2),3,sqrt(2)],[4,sqrt(2),1]])
+aj, p = jacobi(test_array)
+# npprint(aj)
+# npprint(p)
 
 e, a = gen_rand_herm(3)
+npprint(e)
+npprint(a)
+npprint(a.conj().T)
+print(is_hermitian(a))
 
-aj, p = jacobi(a)
-
-npprint(aj)
-npprint(p)
+s = hermitian_matrix_split(a)
+npprint(s)
+print(is_hermitian(s))
