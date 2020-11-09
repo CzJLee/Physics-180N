3726dd5a7db13474dd48914a4e1f414278b3fbf4
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-11-08 18:21:08 -0800

    Working hermitian_eigensystem for hermitians

--- Project 2/proj_2_module.py
+++ Project 2/proj_2_module.py
@@ -3,36 +3,6 @@ from numpy.linalg import norm
 from math import sqrt
 from copy import copy
 
-def hermitian_eigensystem(H, tolerance):
-	"""
-	Solves for the eigenvalues and eigenvectors of a hermitian matrix
-	
-	Args:
-		H: Hermitian matrix for which we want to compute eigenvalues and eigenvectors
-		
-		tolerance: A number that sets the tolerance for the accuracy of the computation.  This number
-		is multiplied by the norm of the matrix H to obtain a number delta.  The algorithm successively
-		applies (via similarity transformation) Jacobi rotations to the matrix H until the sum of the
-		squares of the off-diagonal elements are less than delta.
-	
-	Returns:
-		d: Numpy array containing eigenvalues of H in non-decreasing order
-		
-		U: A 2d numpy array whose columns are the eigenvectors corresponding to the computed
-		eigenvalues.
-		
-	Checks you might need to do:
-		
-		H * U[:,k] = d[k] *　U[:,k]      k=0,1,2,...,(n-1)
-		
-		d[0] <= d[1] <= ... <= d[n-1]     (where n is the dimension of H)
-	   
-		np.transpose(U) * U = U * np.transpose(U) = np.eye(n)
-		
-	"""
-	
-	return d, U
-
 def jacobi(a, tol = 1.0e-9):
 	"""
 	Jacobi Method to Diagonalize Matrices.
@@ -176,3 +146,57 @@ def hermitian_matrix_split(h):
 	s_bot = np.concatenate((b, a), axis = 1)
 	s = np.concatenate((s_top, s_bot), axis = 0)
 	return s
+
+def sort_eigen_pair(w, v):
+	# Get order to sort the eigenvalues and eigenvectors in
+	order = w.argsort()
+	# Sort eigenvalues
+	w = w[order]
+	# Sort eigenvectors by column
+	v = v.T[order].T
+	return w, v
+
+def hermitian_eigensystem(H, tolerance = 1.0e-9):
+	"""
+	Solves for the eigenvalues and eigenvectors of a hermitian matrix
+	
+	Args:
+		H: Hermitian matrix for which we want to compute eigenvalues and eigenvectors
+		
+		tolerance: A number that sets the tolerance for the accuracy of the computation.  This number
+		is multiplied by the norm of the matrix H to obtain a number delta.  The algorithm successively
+		applies (via similarity transformation) Jacobi rotations to the matrix H until the sum of the
+		squares of the off-diagonal elements are less than delta.
+	
+	Returns:
+		d: Numpy array containing eigenvalues of H in non-decreasing order
+		
+		U: A 2d numpy array whose columns are the eigenvectors corresponding to the computed
+		eigenvalues.
+		
+	Checks you might need to do:
+		
+		H * U[:,k] = d[k] *　U[:,k]      k=0,1,2,...,(n-1)
+		
+		d[0] <= d[1] <= ... <= d[n-1]     (where n is the dimension of H)
+	   
+		np.transpose(U) * U = U * np.transpose(U) = np.eye(n)
+		
+	"""
+	n = len(H)
+
+	a = hermitian_matrix_split(H)
+
+	w, v = jacobi(a, tolerance)
+
+	# Slice w, v
+	w = w[:n]
+	v_real = v[:n, :n]
+	v_imag = v[n:2*n, :n]
+	v = v_real + v_imag * 1.0j
+
+	w, v = sort_eigen_pair(w, v)
+
+	return w, v
+	
+	# return d, U
--- Project 2/test2.py
+++ Project 2/test2.py
@@ -1,7 +1,7 @@
 from proj_2_EigenTest import gen_rand_herm, npprint, is_hermitian
 import numpy as np
 from math import sqrt
-from proj_2_module import jacobi, hermitian_matrix_split
+from proj_2_module import jacobi, hermitian_matrix_split, sort_eigen_pair, hermitian_eigensystem
 
 
 
@@ -13,9 +13,26 @@ aj, p = jacobi(test_array)
 e, a = gen_rand_herm(3)
 npprint(e)
 npprint(a)
-npprint(a.conj().T)
-print(is_hermitian(a))
 
-s = hermitian_matrix_split(a)
-npprint(s)
-print(is_hermitian(s))
+w, v = hermitian_eigensystem(a)
+print("My Function w")
+npprint(w)
+print("My Function v")
+npprint(v)
+my_v = v
+
+print()
+w, v = np.linalg.eigh(a)
+print("Numpy w")
+npprint(w)
+print("Numpy v")
+npprint(v)
+
+np_v = v
+
+x = np_v.T[0]
+npprint(x)
+s = 0
+for i in x:
+	s += np.abs(i)
+print(s)
