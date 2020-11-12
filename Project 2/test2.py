9f0b9fea3604a0c4a5ff842faa5d75288d7563b0
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-11-12 12:41:07 -0800

    Testing hermval

--- Project 2/test2.py
+++ Project 2/test2.py
@@ -6,21 +6,25 @@ from proj_2_module import jacobi, hermitian_matrix_split, sort_eigen_pair, hermi
 import time
 
 
-for dim in range(20, 21):
-	e, a = gen_rand_herm(dim)
-	tstart = time.time()
-	w, v = hermitian_eigensystem(a)
-	tend = time.time()
-	eigenvalues_match = np.allclose(w, np.sort(e))
-	eigenvectors_match = verify_eigenvectors(a, w, v)
-	if eigenvalues_match and eigenvectors_match:
-		dtime = tend - tstart
-		print(f"Diagonalized {dim}x{dim} Hermitian matrix in {dtime:.6f} seconds.")
-	else:
-		print(f"Failed to diagonalize {dim}x{dim} Hermitian matrix.")
-		print("Do eigenvalues match?")
-		print(np.allclose(np.sort(e), w))
-		npprint(np.sort(e))
-		npprint(w)
-		print("Do eigenvectors match?")
-		print(eigenvectors_match)
+dim = 4
+
+e, a = gen_rand_herm(dim)
+tstart = time.time()
+w, v = hermitian_eigensystem(a)
+tend = time.time()
+eigenvalues_match = np.allclose(w, np.sort(e))
+eigenvectors_match = verify_eigenvectors(a, w, v)
+if eigenvalues_match and eigenvectors_match:
+	dtime = tend - tstart
+	print(f"Diagonalized {dim}x{dim} Hermitian matrix in {dtime:.6f} seconds.")
+else:
+	print(f"Failed to diagonalize {dim}x{dim} Hermitian matrix.")
+	print("Do eigenvalues match?")
+	print(np.allclose(np.sort(e), w))
+	npprint(np.sort(e))
+	npprint(w)
+	print("Do eigenvectors match?")
+	print(eigenvectors_match)
+
+
+print(np.polynomial.hermite.hermval(1, [1, 1, 1]))
