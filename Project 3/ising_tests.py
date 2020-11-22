ca34e5b1cc96608816167d809894c4d327549f34
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-11-22 12:05:25 -0800

    testing large latices

--- Project 3/ising_tests.py
+++ Project 3/ising_tests.py
@@ -2,8 +2,8 @@ from proj_3 import Ising_2d, weighted_die
 import numpy as np
 import matplotlib.pyplot as plt
 
-num_steps = 100000
-L = 16
+num_steps = 500000
+L = 64
 time_step_size = num_steps / (L ** 2)
 
 # Set temperature to be 3. This is larger than the critical temperature. 
@@ -11,13 +11,17 @@ temp = 3
 
 # Create our Ising model using L = 16, temp = 3.
 model = Ising_2d(L, temp)
+print(model.energy())
 # model.set_rand_state()
 
 # Let's run the MCMC for multiple steps, and calculate values for U and M for each step. 
-vals = model.mcmcm(num_steps, calculate_vals = True)
+vals = model.mcmcm(num_steps, calculate_vals = True, verbose = True)
 
 # Now lets create our list of time steps to plot
 time_steps = [time_step_size * i for i in range(1, num_steps + 1)]
 
-plt.plot(time_steps, vals["M"][1::])
+plt.plot(time_steps, vals["M"])
 plt.show()
+
+print()
+print(model.energy())
