0e97ed4d8fa232a4962fdcffe7ed7018ecf00d0f
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-12-11 13:48:58 -0800

    Restructure to add all self variables to init

--- Project 4/proj_4.py
+++ Project 4/proj_4.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Diffusion_1D():
-	def __init__(self, a = (1.10407 ** 0.5), l = 20, b = 1/6, h = 1):
+	def __init__(self, a = (1.10407 ** 0.5), l = 20, b = 1/6, h = 1, t_max = 120):
 		# Diffusivity ⍺ (cm * s^-1/2)
 		self.a = a 
 
@@ -21,25 +21,40 @@ class Diffusion_1D():
 		# 𝚫t = β * h^2 / ⍺^2
 		self.dt = self.b * self.h**2 / self.a**2
 
+		# Total time to run simulation for (s)
+		self.t_max = t_max
+
+		# Max integer number of length steps (+1 to account for both ends)
+		self.m_max = int((self.l // self.h) + 1)
+
+		# Max integer number of time steps
+		self.n_max = int(self.t_max // self.dt)
+
+		# Create array of time elements
+		self.time = np.arange(0, self.t_max, self.dt)
+
+		# Create array of length elements
+		self.length = np.arange(self.m_max) * self.h
+
 	def set_boundary_conditions(self, temp = 100, bc_left = 0, bc_right = 0):
-		# Create an array with length = (l // h) + 1 
-		# (+1 to account for both ends)
-		# Element of index m in the array will represent the temperature at position x = m * h
-		self.temp = np.ones(((self.l // self.h) + 1, 1)) * temp
-		self.temp[0, 0] = bc_left
-		self.temp[-1, 0] = bc_right
 		self.bc_left = bc_left
 		self.bc_right = bc_right
 
-	def simulate_diffusion(self, t_max = 120):
-		# Create array of time elements
-		self.time = np.arange(0, t_max, self.dt)
+		# Create an array with length = (l // h) + 1 
+		# (+1 to account for both ends)
+		# Element of index m in the array will represent the temperature at position x = m * h
+		self.temp = np.ones((self.m_max, 1)) * temp
+		self.temp[0, 0] = self.bc_left
+		self.temp[-1, 0] = self.bc_right
+		
 
+	def simulate_diffusion(self):
+		# Array and code is formatted to match notation of equation (5)
 		# For each time step, apply equation (5) and store results as a new entry in self.temp
-		for n in range(int(t_max // self.dt)):
-			# Append a column to right for next time step
-			self.temp = np.append(self.temp, np.empty(((self.l // self.h) + 1, 1)), axis = 1)
-
+		for n in range(self.n_max):
+			# Iterate over all time steps
+			# Append an empty column to right of temp for next time step
+			self.temp = np.append(self.temp, np.empty((self.m_max, 1)), axis = 1)
 
 			if self.bc_left is not None:
 				# If there is a left boundary condition
@@ -57,7 +72,9 @@ class Diffusion_1D():
 				# BC is set to None, no BC, apply modified equation 5
 				self.temp[-1, n + 1] = self.temp[-1, n] + self.b * (self.temp[-2, n] - self.temp[-1, n])
 
-			for m in range(1, int(self.l // self.h)):
+			# Apply equation (5) to each non-edge length segment.
+			for m in range(1, self.m_max - 1):
+				# Iterate over all inner length segments
 				self.temp[m, n + 1] = self.temp[m, n] + self.b * (self.temp[m + 1, n] + self.temp[m - 1, n] - 2 * self.temp[m, n])
 
 	def get_temp_at_time(self, n):
@@ -66,17 +83,10 @@ class Diffusion_1D():
 model = Diffusion_1D()
 model.set_boundary_conditions()
 model.simulate_diffusion()
-# print(model.dt)
-print(model.temp)
-# print(model.time)
+
 
 n = int(75 // model.dt)
 blah = model.get_temp_at_time(n)
-print(blah)
 
-lengths = np.arange(0, 21, 1)
-print(lengths)
-
-
-plt.plot(lengths, blah)
+plt.plot(model.length, blah)
 plt.show()
